77ff63904a4fa343118fc79b41b6b94473da4bb1
joliest <joliverestampador@gmail.com>
2017-09-15 21:47:24 +0800

    Added EditText

--- JavaAndroid/MyApplication/app/src/main/java/com/example/joliver_pc/basicinterface/MainActivity.java
+++ JavaAndroid/MyApplication/app/src/main/java/com/example/joliver_pc/basicinterface/MainActivity.java
@@ -15,21 +15,41 @@ public class MainActivity extends AppCompatActivity {
         RelativeLayout myLayout = new RelativeLayout(this);
         myLayout.setBackgroundColor(Color.LTGRAY);
 
-        //Layout parameters is just a rules that you want to do
-        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-        );
-        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
-
         //create a button
         Button button = new Button(this);
         button.setText("Click Me");
         button.setBackgroundColor(Color.GREEN);
 
+        //text field
+        EditText text = new EditText(this);
+
+        //set the Id to arrange the text and button
+        button.setId(1);
+        text.setId(2);
+
+
+        //Layout parameters is just a rules that you want to do
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        RelativeLayout.LayoutParams textDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        textDetails.addRule(RelativeLayout.ABOVE, button.getId());
+        textDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        //margin above button
+        textDetails.setMargins(0,0,0,5); //left, top, right, bottom
+
         //adding the widget using addView() method
         myLayout.addView(button, buttonDetails);
+        myLayout.addView(text, textDetails);
 
         setContentView(myLayout);
 
