418ff89803099daaccfd661261f6341097f01343
joliest <joliverestampador@gmail.com>
2017-09-16 01:53:14 +0800

    Gesture - not completed

---
+++ JavaAndroid/Gestures/app/src/main/java/com/example/joliver_pc/gestures/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.joliver_pc.gestures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.GestureDetector.*;
+import android.view.MotionEvent;
+import android.widget.TextView;
+import android.support.v4.view.GestureDetectorCompat
+
+public class MainActivity extends AppCompatActivity implements OnGestureListener, GestureDetector.OnDoubleTapListener{
+
+    private TextView joliverMessage;
+    private GestureDetectorCompat gestureDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joliverMessage = (TextView) findViewById(R.id.joliverMessage); // get the TextView from the xml file
+        this.gestureDetector = new GestureDetectorCompat(this, this);  // detects Gesture on this screen
+        gestureDetector.setOnDoubleTapListener(this);                  // detects double tap
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureDetector.onTouchEvent(event); //pass the event into GestureDetectorCompat
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+}
