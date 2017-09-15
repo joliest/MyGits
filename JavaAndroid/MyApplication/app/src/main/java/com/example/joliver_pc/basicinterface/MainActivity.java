aaac976c7951c6cbeaa17e68e74aa081964353c1
joliest <joliverestampador@gmail.com>
2017-09-15 23:04:14 +0800

    Converting DIP to PX
    // DCP allows you to make the size of the widget to appear the same to different screen sizes

--- JavaAndroid/MyApplication/app/src/main/java/com/example/joliver_pc/basicinterface/MainActivity.java
+++ JavaAndroid/MyApplication/app/src/main/java/com/example/joliver_pc/basicinterface/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.joliver_pc.basicinterface;
 
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
@@ -42,10 +44,19 @@ public class MainActivity extends AppCompatActivity {
         buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
         buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
 
+
+        //convert DCP(Density/Device independent pixels) to px -
+        // DCP allows you to make the size of the widget to appear the same to different screen sizes
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200, r.getDisplayMetrics());
+
+        //setWidth() only accepts "px"
+        text.setWidth(px);
+
         textDetails.addRule(RelativeLayout.ABOVE, button.getId());
         textDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        //margin above button
-        textDetails.setMargins(0,0,0,5); //left, top, right, bottom
+
+        textDetails.setMargins(0,0,0,5); //margin above button (bottom margin, setMargins(l, t, r, b))
 
         //adding the widget using addView() method
         myLayout.addView(button, buttonDetails);
