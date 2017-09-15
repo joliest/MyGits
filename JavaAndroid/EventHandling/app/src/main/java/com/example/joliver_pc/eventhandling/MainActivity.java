0f142e70920eac2a320c55a7630be136679c955b
joliest <joliverestampador@gmail.com>
2017-09-15 23:32:15 +0800

    OnClick and LongOnClick

--- JavaAndroid/EventHandling/app/src/main/java/com/example/joliver_pc/eventhandling/MainActivity.java
+++ JavaAndroid/EventHandling/app/src/main/java/com/example/joliver_pc/eventhandling/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.joliver_pc.eventhandling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button joliverButton = (Button) findViewById(R.id.joliverButton);
+
+        joliverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText joliverText = (EditText) findViewById(R.id.joliverText);
+                joliverText.setText("Oh Yeah");
+            }
+        });
+
+        joliverButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                EditText joliverText = (EditText) findViewById(R.id.joliverText);
+                joliverText.setText("Longggg hold baby!");
+
+                //tells the compiler to let the longOnClick value to stay
+                return true;
+            }
+        });
     }
 }
