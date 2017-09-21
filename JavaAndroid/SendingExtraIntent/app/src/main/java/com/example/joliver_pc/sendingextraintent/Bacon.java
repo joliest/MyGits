9a6c1bb1733df3dd6e19dc47136d6902abba4142
joliest <joliverestampador@gmail.com>
2017-09-22 03:11:18 +0800

    ExtraIntent

---
+++ JavaAndroid/SendingExtraIntent/app/src/main/java/com/example/joliver_pc/sendingextraintent/Bacon.java
@@ -0,0 +1,15 @@
+package com.example.joliver_pc.sendingextraintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bacon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacon);
+
+
+    }
+}
