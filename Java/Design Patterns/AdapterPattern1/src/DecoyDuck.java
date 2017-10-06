06f4ca0bd164c695a80aeef43a1e39db6cf160db
joliest <joliverestampador@gmail.com>
2017-10-06 18:51:16 +0800

    Adapter Patterns

---
+++ Java/Design Patterns/AdapterPattern1/src/DecoyDuck.java
@@ -0,0 +1,12 @@
+
+public class DecoyDuck implements Duck{
+
+	public void quack() {
+		System.out.println("Deciy's quack quack");
+	}
+	
+	public void fly() {
+		System.out.println("Decoy's fly fly and swim");
+	}
+	
+}
