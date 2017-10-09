bbf16a9811856f0b560f4682e6b7ca39cca933ef
joliest <joliverestampador@gmail.com>
2017-10-10 05:32:21 +0800

    Mini Tracker in Taguig

---
+++ Java/Project_JIRA/MiniTracker/src/com/tracker/Module.java
@@ -0,0 +1,10 @@
+package com.tracker;
+
+import javax.servlet.http.*;
+
+public interface Module {
+	int createId();
+	void setHttpRequest(HttpServletRequest request);
+	HttpServletRequest getHttpRequest();
+	void executeCommand();
+}
