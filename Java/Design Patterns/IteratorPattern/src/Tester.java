985479a0ad60c421272156df531767337fefe21d
joliest <joliverestampador@gmail.com>
2017-10-24 01:44:56 +0800

    Added Hashtable CafeMenu

--- Java/Design Patterns/IteratorPattern/src/Tester.java
+++ Java/Design Patterns/IteratorPattern/src/Tester.java
@@ -3,8 +3,9 @@ public class Tester {
 	public static void main(String[] args) {
 		Menu breakfast = new PancakeHouseMenu();
 		Menu lunch = new DinerMenu();
+		Menu dinner = new CafeMenu();
 		
-		Waitress waitress = new Waitress(breakfast, lunch);
+		Waitress waitress = new Waitress(breakfast, lunch, dinner);
 		waitress.printMenu();
 	}
 }
--- Java/Design Patterns/IteratorPattern/src/Waitress.java
+++ Java/Design Patterns/IteratorPattern/src/Waitress.java
@@ -4,21 +4,27 @@ import java.util.*;
 public class Waitress {
 	Menu pancakeMenu;
 	Menu dinerMenu;
+	Menu cafeMenu;
 	
-	public Waitress(Menu pancakeMenu, Menu dinerMenu) {
+	public Waitress(Menu pancakeMenu, Menu dinerMenu, Menu cafeMenu) {
 		this.pancakeMenu = pancakeMenu;
 		this.dinerMenu = dinerMenu;
+		this.cafeMenu = cafeMenu;
 	}
 	
 	public void printMenu() {
 		Iterator pancakeMenuIterator = pancakeMenu.createIterator();
 		Iterator dinerMenuIterator = dinerMenu.createIterator();
+		Iterator cafeMenuIterator = cafeMenu.createIterator();
 		
 		System.out.println("Breakfast: ");
 		printMenu(pancakeMenuIterator);
 		
 		System.out.println("Lunch: ");
 		printMenu(dinerMenuIterator);
+		
+		System.out.println("Dinner: ");
+		printMenu(cafeMenuIterator);
 	}
 	
 	public void printMenu(Iterator iterator) {
